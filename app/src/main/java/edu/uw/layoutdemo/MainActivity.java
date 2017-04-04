32498210329fb4368ade607c4af365bdf5274039
tiffany <tzchen@uw.edu>
2017-04-04 15:59:40 -0700

    some changes

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.java
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.java
@@ -2,6 +2,9 @@ package edu.uw.layoutdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LAYOUT_DEMO";
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView img = (ImageView)findViewById(R.id.imgCat);
+                img.setImageResource(R.drawable.android_maneki);
+            }
+        });
+
+
     }
 }
